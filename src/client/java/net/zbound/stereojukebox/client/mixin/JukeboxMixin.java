8b0cec1f439714d429c69a8bfba2a89ac37e94da
WatDuhHekBro <watduhhekbro@zbound.net>
2025-07-29 17:14:56 -0500

    Merged jukebox mixin file

--- src/client/java/net/zbound/stereojukebox/client/mixin/JukeboxMixin.java
+++ src/client/java/net/zbound/stereojukebox/client/mixin/JukeboxMixin.java
@@ -1,14 +1,44 @@
 package net.zbound.stereojukebox.client.mixin;
 
-import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundSystem;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SoundSystem.class)
+public abstract class JukeboxMixin {
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)Lnet/minecraft/client/sound/SoundSystem$PlayResult;", at = @At("HEAD"))
+    private void initInjected(SoundInstance sound, CallbackInfoReturnable<SoundSystem.PlayResult> cir) {
+        if(sound.getCategory() == SoundCategory.RECORDS) {
+            if(sound instanceof AbstractSoundInstanceAccessor soundModifier) {
+                soundModifier.setAttenuationType(SoundInstance.AttenuationType.NONE);
+                soundModifier.setRelative(true);
+                //soundModifier.setVolume(0.1f);
+                //soundModifier.setVolume(16f);
+
+                // NOTE: This sets the sound's location to telport ~5 ~5 ~5 away from the player.
+                // This always plays in the player's right ear.
+                // Attenuation still works actually, it's just not really useful.
+                // If you want to reduce the volume based on distance, you should do that separately.
+                // You'd need to somehow get the player's position.
+
+                //soundModifier.setX(5);
+                //soundModifier.setY(5);
+                //soundModifier.setZ(5);
+
+                // Make sure to set the relative position to the player's position if attenuation is enabled.
+                soundModifier.setX(0);
+                soundModifier.setY(0);
+                soundModifier.setZ(0);
+            }
+
+            //System.out.println(sound.getAttenuationType() + " / " + sound.isRelative() + " / " + sound.getX() + ", " + sound.getY() + " / " + sound.getZ() + " / ");
+        }
+    }
+}
 
 /*@Mixin(Source.class)
 public abstract class JukeboxMixin {
@@ -35,7 +65,7 @@ public abstract class JukeboxMixin {
 // Attempt to convert "record" type to match "music" type params, because apparently music is also a PositionedSoundInstance?!
 // volume = 1.0, notice how "relative = true" for music and ambient, could this be why?
 // If not, then notice how stereo sounds are always non-directional while mono sounds are, which might be a property of the audio stream.
-@Mixin(PositionedSoundInstance.class)
+/*@Mixin(PositionedSoundInstance.class)
 public abstract class JukeboxMixin {
     private SoundInstance.AttenuationType attenuationType;
     private boolean relative;
@@ -56,4 +86,4 @@ public abstract class JukeboxMixin {
             System.out.println(this);
         }
     }
-}
+}*/
